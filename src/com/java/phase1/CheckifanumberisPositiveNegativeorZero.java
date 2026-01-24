d17c20f64d0e389c55dc9ef325d81f9b0e5938fd
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-24 18:57:50 +0530

    phase1

---
+++ src/com/java/phase1/CheckifanumberisPositiveNegativeorZero.java
@@ -0,0 +1,25 @@
+package com.java.phase1;
+
+import java.util.Scanner;
+
+public class CheckifanumberisPositiveNegativeorZero {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		if(n > 0) {
+			System.out.println("Num is positive");
+		} else if(n<0) {
+			System.out.println("Num is negative");
+		} else {
+			System.out.println("Number is zero");
+		}
+
+
+
+	}
+
+
+}
