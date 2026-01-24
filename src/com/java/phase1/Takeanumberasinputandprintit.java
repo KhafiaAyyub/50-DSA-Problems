2b375c6cbbd09cc1a8ba5624d3315f75ad868f78
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-24 18:49:42 +0530

    phase1

---
+++ src/com/java/phase1/Takeanumberasinputandprintit.java
@@ -0,0 +1,15 @@
+package com.java.phase1;
+
+import java.util.Scanner;
+
+public class Takeanumberasinputandprintit {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println("The number is " + n + ".....");
+		
+	}
+}
---
+++ src/com/java/phase1/Taketwonumbersandprinttheirsum.java
@@ -0,0 +1,20 @@
+package com.java.phase1;
+
+import java.util.Scanner;
+
+public class Taketwonumbersandprinttheirsum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		int sum = n1+n2;
+		 System.out.println("sum is :- "+ sum);
+		
+		
+	}
+	
+	
+}
