49395a46a8721bc082927f676fd10d940090f4a3
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-24 19:12:37 +0530

    phase1

---
+++ src/com/java/phase1/Findthemaximumofthreenumbers.java
@@ -0,0 +1,25 @@
+package com.java.phase1;
+
+import java.util.Scanner;
+
+public class Findthemaximumofthreenumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		if(n1>=n2 && n1>=n3) {
+			System.out.println(n1);
+		} else if(n2>=n1 && n2>=n3) {
+			System.out.println(n2);
+		} else {
+			System.out.println(n3);
+		}
+		
+		
+		
+	}
+	
+}
