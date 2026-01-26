2c31c3bf943726c482ffe09f1362f6d45e9cc7ec
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-26 18:47:34 +0530

    phase1

--- src/com/java/phase1/Printnumbersfrom1toN.java
+++ src/com/java/phase1/Printnumbersfrom1toN.java
@@ -1,5 +1,19 @@
 package com.java.phase1;
 
+import java.util.Scanner;
+
 public class Printnumbersfrom1toN {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);		
+		
+		int n= sc.nextInt();
+		
+		
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+		}
+		
+	}
 }
