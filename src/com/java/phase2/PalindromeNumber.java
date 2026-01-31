5519d33a9a9ba0a3d39442258737a46199d41f4e
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-31 17:42:50 +0530

    phase2

--- src/com/java/phase2/PalindromeNumber.java
+++ src/com/java/phase2/PalindromeNumber.java
@@ -1,5 +1,28 @@
 package com.java.phase2;
 
+import java.util.Scanner;
+
 public class PalindromeNumber {
 
+	//123 - 321
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int rev = 0;
+		int num = n;
+		
+		while(n>0) {
+			 int lastDigit = n%10;
+			 rev = rev * 10 + lastDigit;
+			 n = n/10;
+		}
+		
+		if(rev == num) {
+		System.out.println("palindrome number");
+	} else {
+		System.out.println("not palindrome");
+	}
+	
+}
 }
