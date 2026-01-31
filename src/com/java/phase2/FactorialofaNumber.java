60215c5a3bdc2cc9afbc8bc0fbaea15061637cc5
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-31 17:59:50 +0530

    phase2

--- src/com/java/phase2/FactorialofaNumber.java
+++ src/com/java/phase2/FactorialofaNumber.java
@@ -1,12 +1,25 @@
 package com.java.phase2;
 
+import java.util.Scanner;
+
 public class FactorialofaNumber {
 
 //	Input: 5
 //	Output: 120
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
+		int res = 1;
 		
+		for(int i=n;i>=1;i--) {
+			res = res * i ;	
+		}
+		
+		System.out.println("Factorial is: " + res);
 	}
+	
+	
+	
 }
