b1d2c9a46ba488894d6192d65e2b6fe09665bf18
KhafiaAyyub <khafiaayyub@gmail.com>
2026-01-31 18:20:49 +0530

    phase2

--- src/com/java/phase2/FibonacciSeries.java
+++ src/com/java/phase2/FibonacciSeries.java
@@ -1,5 +1,26 @@
 package com.java.phase2;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int first = 0;
+		int second = 1;
+		int n = sc.nextInt();
+		System.out.print(first+ " "+ second + " ");
+		
+		for(int i=2;i<n;i++) {
+			int next = first + second;
+            System.out.print(next + " ");
+			first = second;
+			second = next;
+		}
+
+		
+
+
+	}
 }
