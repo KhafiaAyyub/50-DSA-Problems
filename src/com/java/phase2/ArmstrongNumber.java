e96567d50bbcf88d6fcfd1cf8c1ad869c331e14a
KhafiaAyyub <khafiaayyub@gmail.com>
2026-02-01 18:42:37 +0530

    phase2

--- src/com/java/phase2/ArmstrongNumber.java
+++ src/com/java/phase2/ArmstrongNumber.java
@@ -1,5 +1,8 @@
 package com.java.phase2;
 
+// Sum of each digit raised to the power of number of digits 
+// is equal to the original number.
+
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
